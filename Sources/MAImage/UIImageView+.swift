d4d00c9d4f6e1e1a3fd4621be7eafecf7dbd5239
Murilo Araujo <muernandes@icloud.com>
2020-10-10 23:05:01 -0300

    feat: Implement UIImageView extension

---
+++ Sources/MAImage/UIImageView+.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Murilo Araujo on 10/10/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    public func loadImage(with urlString: String, and placeholderImage: UIImage = UIImage()) {
+        self.image = placeholderImage
+        
+        MAImage.shared.getImageBy(urlString: urlString) { (image) in
+            if let image = image {
+                self.image = image
+            }
+        }
+    }
+}
